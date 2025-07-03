9b24959a30e3f2e35f763f7f83d12ea44f899615
Konstantin Krivopustov <krivopustov@haulmont.com>
2025-07-03 18:00:26 +0400

    Observability with Prometheus, Loki, Grafana

--- src/main/java/io/jmix/ai/backend/security/JmixAiBackendSecurityConfiguration.java
+++ src/main/java/io/jmix/ai/backend/security/JmixAiBackendSecurityConfiguration.java
@@ -1,6 +1,10 @@
 package io.jmix.ai.backend.security;
 
+import io.jmix.core.JmixSecurityFilterChainOrder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -32,4 +36,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class JmixAiBackendSecurityConfiguration {
 
+    @Bean
+    @Order(JmixSecurityFilterChainOrder.CUSTOM)
+    SecurityFilterChain publicFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/actuator/**")
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().permitAll()
+                );
+        return http.build();
+    }
 }
